4005291dbfcfa939ce3b6ce4c84ed20be06084a1
oloyedewaris <oloyedewaris@gmail.com>
2024-07-16 12:34:02 +0100

    changed BACKEND_URL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,20 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const BACKEND_URL = 'https://dg-core-backend-production.up.railway.app/';
-// const BACKEND_URL = 'https://test-wb-1f446534f6a9.herokuapp.com/';
+const BACKEND_URL = "https://dg-backend-9082438dccf0.herokuapp.com/";
 
 const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
-  headers: { 'Content-Type': 'application/json' },
-  timeout: 30 * 60 * 1000
+  headers: { "Content-Type": "application/json" },
+  timeout: 30 * 60 * 1000,
 });
 
 axiosInstance.interceptors.request.use(
   async (config) => {
-    let accessToken = localStorage.getItem('accessToken');
+    let accessToken = localStorage.getItem("accessToken");
 
-    if (accessToken)
-      config.headers['Authorization'] = `Bearer ${accessToken}`
+    if (accessToken) config.headers["Authorization"] = `Bearer ${accessToken}`;
 
     return config;
   },
